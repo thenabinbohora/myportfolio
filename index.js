4fdfb2ee0e46eb2079663dd7a45be78339b68bc2
Nabin <thenabinbohora@gmail.com>
2023-07-18 19:40:13 +0930

    project update

--- index.js
+++ index.js
@@ -3,18 +3,33 @@
 // 
 
 function contact(event) {
-    event.preventDefault();
-        emailjs
-            .sendForm(
-                'service_cqfd01j',
-                'template_jprbwub',
-                event.target,
-                '4VE8jnQgLrxzpjEpf'
+     event.preventDefault();
+     const loading = document.querySelector('.modal__overlay--loading');
+     const success = document.querySelector('.modal__overlay--success');
+     loading.classList += ' modal__overlay--visible';
+    emailjs
+        .sendForm(
+             'service_cqfd01j',
+             'template_jprbwub',
+             event.target,
+             '4VE8jnQgLrxzpjEpf'
             ).then(() => {
-
+                loading.classList.remove("modal__overlay--visible");
+                success.classList += ' modal__overlay--visible';
+            }).catch(() => {
+                loading.classList.remove("modal__overlay--visible");
+                alert (
+                    "The email service is temporarily down. Please reach me directly through thenabinbohora@gmail.com"
+                );
             })
-    const loading = document.querySelector('modal__overlay--loading');
-    const success = document.querySelector('modal__overlay--success');
-    
+}
 
+let isModalOpen = false;
+function toggleModal() {
+    if (isModalOpen) {
+        isModalOpen = false;
+        return document.body.classList.remove("modal--open")
+    }
+    isModalOpen = true;
+    document.body.classList += ' modal--open';
 }
