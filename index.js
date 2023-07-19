ae0fa72d53f56422feeced079ae569a60010e33a
Nabin <thenabinbohora@gmail.com>
2023-07-19 21:04:29 +0930

    project update

--- index.js
+++ index.js
@@ -1,30 +1,52 @@
-// 
-// 
-// 
+let isModalOpen = false;
+let contrastToggle = false;
+let scaleFactor = 1 / 20
 
-function contact(event) {
-     event.preventDefault();
-     const loading = document.querySelector('.modal__overlay--loading');
-     const success = document.querySelector('.modal__overlay--success');
-     loading.classList += ' modal__overlay--visible';
-    emailjs
-        .sendForm(
-             'service_cqfd01j',
-             'template_jprbwub',
-             event.target,
-             '4VE8jnQgLrxzpjEpf'
-            ).then(() => {
-                loading.classList.remove("modal__overlay--visible");
-                success.classList += ' modal__overlay--visible';
-            }).catch(() => {
-                loading.classList.remove("modal__overlay--visible");
-                alert (
-                    "The email service is temporarily down. Please reach me directly through thenabinbohora@gmail.com"
-                );
-            })
+function moveBackground(event) {
+    const shapes = document.querySelectorAll(".shape");
+    const x = event.clientX * scaleFactor;
+    const y = event.clientY * scaleFactor;
+    for (let i = 0; i < shapes.length; i++) {
+        const isOdd = i % 2 !== 0;
+        const boolOdd = isOdd ? -1 : 1;
+        shapes[i].style.transform = `translate(${x * boolOdd}px, ${y * boolOdd}px)`;
+    }
 }
 
-let isModalOpen = false;
+function toggleContrast() {
+    contrastToggle = !contrastToggle;
+    if (contrastToggle) {
+        document.body.classList += " dark-theme";
+    }
+
+    else {
+        document.body.classList.remove("dark-theme");
+    }
+}
+
+function contact(event) {
+    event.preventDefault();
+    const loading = document.querySelector('.modal__overlay--loading');
+    const success = document.querySelector('.modal__overlay--success');
+    loading.classList += ' modal__overlay--visible';
+    emailjs
+        .sendForm(
+            'service_cqfd01j',
+            'template_jprbwub',
+            event.target,
+            '4VE8jnQgLrxzpjEpf'
+        ).then(() => {
+            loading.classList.remove("modal__overlay--visible");
+            success.classList += ' modal__overlay--visible';
+        }).catch(() => {
+            loading.classList.remove("modal__overlay--visible");
+            alert(
+                "The email service is temporarily down. Please reach me directly through thenabinbohora@gmail.com"
+            );
+        })
+}
+
+
 function toggleModal() {
     if (isModalOpen) {
         isModalOpen = false;
